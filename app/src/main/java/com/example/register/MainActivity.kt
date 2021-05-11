950b7a4ef55cb28b0dbd3a55e606a8c5f2fda3c8
SNKz0610 <blackaxe2202@gmail.com>
2021-05-11 17:37:26 +0700

    chore(): task#1105 Adding function for EditText email and Button continue. I mean when app running, user cant click continue button and its color is grey. When they typing something in the email edittext, the button will become clickable and turn into re color. Closes task#1105

--- app/src/main/java/com/example/register/MainActivity.kt
+++ app/src/main/java/com/example/register/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.example.register
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 import com.example.register.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnContinue : Button = findViewById(R.id.btn_continue)
+        btnContinue.setBackgroundColor(resources.getColor(R.color.grey))
+        val edtEmail : EditText = findViewById(R.id.edt_email)
+
+        edtEmail.addTextChangedListener(object :TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                btnContinue.isClickable = false
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val redColor : String = "#ED3B3B"
+                btnContinue.isClickable = true
+                btnContinue.setBackgroundColor(resources.getColor(R.color.red))
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                btnContinue.isClickable = true
+            }
+        })
+
     }
 }
